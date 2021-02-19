5bedb9fd306a07f11c944c769d1b30124914d949
Doug <git@pixlwave.uk>
2021-02-19 17:23:03 +0000

    Initial commit.

---
+++ Sources/App/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import AVFoundation
+
+struct ContentView: View {
+    var body: some View {
+        Waveform()
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sources/Waveform/Waveform.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+import AVFoundation
+
+struct Waveform: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
