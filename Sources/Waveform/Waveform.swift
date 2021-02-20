4493c56c240a29865c9fce2af268993b60d9a373
Doug <git@pixlwave.uk>
2021-02-20 09:14:51 +0000

    Remove displayScale as Path doesn't seem to handle fractional points as well as CGPath.

--- Sources/Waveform/Waveform.swift
+++ Sources/Waveform/Waveform.swift
@@ -8,7 +8,6 @@ struct Waveform: View {
     @State private var waveformData: [WaveformData] = []
     
     @State private var frameSize: CGSize = .zero
-    @Environment(\.displayScale) var displayScale
     
     init(audioFile: AVAudioFile) {
         self.audioFile = audioFile
@@ -22,7 +21,7 @@ struct Waveform: View {
     
     var body: some View {
         GeometryReader { geometry in
-            WaveformRenderer(waveformData: $waveformData, displayScale: displayScale)
+            WaveformRenderer(waveformData: $waveformData)
                 .preference(key: SizeKey.self, value: geometry.size)
         }
         .onPreferenceChange(SizeKey.self) {
@@ -42,12 +41,12 @@ struct Waveform: View {
             
             let channels = Int(buffer.format.channelCount)
             let length = Int(buffer.frameLength)
-            let samplesPerPoint = length / Int(width * displayScale)
+            let samplesPerPoint = length / Int(width)
             
             #warning("handle ints if necessary")
             guard let floatChannelData = buffer.floatChannelData else { return }
             
-            DispatchQueue.concurrentPerform(iterations: Int(width * displayScale)) { point in
+            DispatchQueue.concurrentPerform(iterations: Int(width)) { point in
                 var data: WaveformData = .zero
                 for sample in 0..<samplesPerPoint {
                     for channel in 0..<channels {
--- Sources/Waveform/WaveformRenderer.swift
+++ Sources/Waveform/WaveformRenderer.swift
@@ -2,20 +2,19 @@ import SwiftUI
 
 struct WaveformRenderer: Shape {
     @Binding var waveformData: [WaveformData]
-    let displayScale: CGFloat
     
     func path(in rect: CGRect) -> Path {
         Path { path in
             path.move(to: CGPoint(x: 0, y: rect.midY))
             
             for index in 0..<waveformData.count {
-                let x = CGFloat(index) / displayScale
+                let x = CGFloat(index)
                 let max = rect.midY + (rect.midY * CGFloat(waveformData[index].max))
                 path.addLine(to: CGPoint(x: x, y: max))
             }
             
             for index in (0..<waveformData.count).reversed() {
-                let x = CGFloat(index) / displayScale
+                let x = CGFloat(index)
                 let min = rect.midY + (rect.midY * CGFloat(waveformData[index].min))
                 path.addLine(to: CGPoint(x: x, y: min))
             }
